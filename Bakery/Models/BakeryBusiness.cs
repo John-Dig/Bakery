849a2cf4e29e9f1fc2855e6c09776762dbb0b0dc
John Diggins <rohnfiggins@gmail.com>
2023-02-24 09:43:32 -0800

    1 passed

--- Bakery/Models/BakeryBusiness.cs
+++ Bakery/Models/BakeryBusiness.cs
@@ -1,9 +1,15 @@
-//using System;
+using System;
 
 namespace Bakery.Models
 {
-  public class Boulangerie //?
+  public class Boulangerie //the class for the whole bakery
   {
+    public string CheckoutMessage { get; set; }
+    public float OrderTotal { get; set; }
+
+    // public .. Bread
+    // public .. Pastry
+
 
   }
 }
